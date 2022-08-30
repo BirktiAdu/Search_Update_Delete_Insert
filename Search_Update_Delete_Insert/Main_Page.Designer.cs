b873b323f0a9aa78e2bc522cee1ddcf98480aa89
BirktiAdu <desalegnbiruktawit9487@gmail.com>
2022-08-30 20:15:48 +0300

    Add project files.

---
+++ Search_Update_Delete_Insert/Main_Page.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Search_Update_Delete_Insert
+{
+    partial class Main_Page
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.tb_firstname = new System.Windows.Forms.TextBox();
+            this.tb_lastname = new System.Windows.Forms.TextBox();
+            this.tb_email = new System.Windows.Forms.TextBox();
+            this.tb_address = new System.Windows.Forms.TextBox();
+            this.tb_phoneno = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FIRST NAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "LAST NAME";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(65, 214);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "EMAIL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(65, 258);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "ADDRESS";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(65, 308);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(92, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "PHONE NO";
+            // 
+            // tb_id
+            // 
+            this.tb_id.Location = new System.Drawing.Point(218, 54);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(383, 26);
+            this.tb_id.TabIndex = 6;
+            // 
+            // tb_firstname
+            // 
+            this.tb_firstname.Location = new System.Drawing.Point(218, 112);
+            this.tb_firstname.Name = "tb_firstname";
+            this.tb_firstname.Size = new System.Drawing.Size(383, 26);
+            this.tb_firstname.TabIndex = 7;
+            // 
+            // tb_lastname
+            // 
+            this.tb_lastname.Location = new System.Drawing.Point(218, 165);
+            this.tb_lastname.Name = "tb_lastname";
+            this.tb_lastname.Size = new System.Drawing.Size(383, 26);
+            this.tb_lastname.TabIndex = 8;
+            // 
+            // tb_email
+            // 
+            this.tb_email.Location = new System.Drawing.Point(218, 214);
+            this.tb_email.Name = "tb_email";
+            this.tb_email.Size = new System.Drawing.Size(383, 26);
+            this.tb_email.TabIndex = 9;
+            // 
+            // tb_address
+            // 
+            this.tb_address.Location = new System.Drawing.Point(218, 258);
+            this.tb_address.Name = "tb_address";
+            this.tb_address.Size = new System.Drawing.Size(383, 26);
+            this.tb_address.TabIndex = 10;
+            // 
+            // tb_phoneno
+            // 
+            this.tb_phoneno.Location = new System.Drawing.Point(218, 305);
+            this.tb_phoneno.Name = "tb_phoneno";
+            this.tb_phoneno.Size = new System.Drawing.Size(383, 26);
+            this.tb_phoneno.TabIndex = 11;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(651, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(512, 384);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Location = new System.Drawing.Point(711, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(320, 35);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Show Data";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(1, 361);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 37);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Coral;
+            this.button3.Location = new System.Drawing.Point(183, 361);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 37);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button4.Location = new System.Drawing.Point(347, 361);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(145, 37);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Insert";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Red;
+            this.button5.Location = new System.Drawing.Point(515, 361);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(123, 37);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Delete";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Main_Page
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1175, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tb_phoneno);
+            this.Controls.Add(this.tb_address);
+            this.Controls.Add(this.tb_email);
+            this.Controls.Add(this.tb_lastname);
+            this.Controls.Add(this.tb_firstname);
+            this.Controls.Add(this.tb_id);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Name = "Main_Page";
+            this.Text = "Main_Page";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.TextBox tb_firstname;
+        private System.Windows.Forms.TextBox tb_lastname;
+        private System.Windows.Forms.TextBox tb_email;
+        private System.Windows.Forms.TextBox tb_address;
+        private System.Windows.Forms.TextBox tb_phoneno;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
---
+++ Search_Update_Delete_Insert/Main_Page.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Search_Update_Delete_Insert
+{
+    public partial class Main_Page : Form
+    {
+        public Main_Page()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = " Data Source =DESKTOP-U75EESK\\SQLEXPRESS;Initial Catalog = c#;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            conn.Open();
+
+            int Id = int.Parse(tb_id.Text);
+            String FirstName = tb_firstname.Text;
+            String LastName = tb_lastname.Text;
+            String Email = tb_email.Text;
+            String Address = tb_address.Text;
+            String PhoneNo = tb_phoneno.Text;
+           
+          
+
+            string Query = "  INSERT INTO MAIN_PAGE(ID, FIRSTNAME, LASTNAME, EMAIL, ADDRESS, PHONENO)VALUES("+Id+", '"+FirstName+"', '"+LastName+"', '"+Email+"', '"+Address+"', "+PhoneNo+")";
+            SqlCommand cmd = new SqlCommand(Query, conn);
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            MessageBox.Show("DATA SAVED SUCCESSFULLY");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = " Data Source =DESKTOP-U75EESK\\SQLEXPRESS;Initial Catalog = c#;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            conn.Open();
+
+            String NameId = tb_id.Text;
+            String FirstName = tb_firstname.Text;
+            String LastName = tb_lastname.Text;
+            String Email = tb_email.Text;
+            String Address = tb_address.Text;
+            String PhoneNo = tb_phoneno.Text;
+
+
+
+            string Query = " SELECT*FROM MAIN_PAGE";
+            SqlCommand cmd = new SqlCommand(Query, conn);
+
+            var reader = cmd.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+
+            dataGridView1.DataSource = table;
+            
+
+            conn.Close();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = " Data Source =DESKTOP-U75EESK\\SQLEXPRESS;Initial Catalog = c#;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            conn.Open();
+
+            String NameId = tb_id.Text;
+            String FirstName = tb_firstname.Text;
+            String LastName = tb_lastname.Text;
+            String Email = tb_email.Text;
+            String Address = tb_address.Text;
+            String PhoneNo = tb_phoneno.Text;
+
+
+
+            string Query = " UPDATE MAIN_PAGE SET FIRSTNAME = '"+FirstName+ "', LASTNAME ='" + LastName + "', EMAIL = '" + Email + "', ADDRESS = '" + Address + "', PHONENO = " + PhoneNo + " WHERE ID = " + NameId;
+            //
+            SqlCommand cmd = new SqlCommand(Query, conn);
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+            MessageBox.Show("DATA SAVED UPDATED");
+
+            tb_id.Text = "";
+            tb_firstname.Text = " ";
+            tb_lastname.Text= " ";
+            tb_email.Text = "";
+            tb_address.Text = "";
+            tb_phoneno.Text = "";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = " Data Source =DESKTOP-U75EESK\\SQLEXPRESS;Initial Catalog = c#;Integrated Security=True";
+            SqlConnection conn = new SqlConnection(ConnectionString);
+            conn.Open();
+
+            String NameId = tb_id.Text;
+          
+
+
+
+            string Query = "DELETE FROM MAIN_PAGE WHERE ID = "+NameId;
+            SqlCommand cmd = new SqlCommand(Query, conn);
+
+            var reader = cmd.ExecuteReader();
+            DataTable table = new DataTable();
+            table.Load(reader);
+
+            dataGridView1.DataSource = table;
+
+
+            conn.Close();
+        }
+    }
+}
